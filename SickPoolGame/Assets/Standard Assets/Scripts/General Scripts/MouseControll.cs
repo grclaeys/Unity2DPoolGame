61a63a7a410068590dbc58bb99cf502aa74a2d31
grclaeys <grclaeys@gmail.com>
2015-11-13 13:15:01 -0800

    boom
    fixed
    -thomas

---
+++ SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/MouseControll.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseControll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mouse = Input.mousePosition;
+        mouse = mouse * 10F;
+        transform.position = Camera.main.ScreenToViewportPoint(mouse);
+    }
+}
