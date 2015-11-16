c0112fbb99ecfdea103f6f47b76bbe08655523da
grclaeys <grclaeys@gmail.com>
2015-11-16 13:03:10 -0800

    whoops

---
+++ SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/MouseControll.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseControll : MonoBehaviour {
+
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+
+    // Use this for initialization
+    void Start () {
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        // Xd();
+    }
+
+    void Xd () {
+      //  while (true) {
+            
+        //}
+    }
+}
