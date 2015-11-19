44afe5caf244e697da107c94c533d12851486f90
grclaeys <grclaeys@gmail.com>
2015-11-19 12:46:18 -0800

    such pressure

--- SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/BallCollision.cs
+++ SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/BallCollision.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class BallCollision : MonoBehaviour {
 
+    double speed = 3.0;
 	// Use this for initialization
 	void Start () {
-	
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
 }
---
+++ SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/RotateCue.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCue : MonoBehaviour {
+    private float degree;
+    private float angle;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1"))
+        {
+            transform.Rotate(0, 0, 1.2f);
+
+        }
+
+        if (Input.GetButton("Fire2"))
+        {
+            transform.Rotate(0, 0, -1.2f);
+           
+        }
+    }
+
+  
+}
