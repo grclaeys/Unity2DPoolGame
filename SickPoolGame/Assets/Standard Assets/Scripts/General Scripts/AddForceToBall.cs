f0f2654be4da4a471b6cd2a0e76e8062c8bce3b2
grclaeys <grclaeys@gmail.com>
2015-11-20 13:11:20 -0800

    not a whole lot :(

---
+++ SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/AddForceToBall.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddForceToBall : MonoBehaviour {
+    //Collision col = Collision.gameObject;
+    public float thrust;
+    public Rigidbody rb;
+    public float hoverForce;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //OnCollisionEnter(Collision.gameObject);
+        //rb.AddForce(transform.forward * thrust);
+        other.rigidbody.AddForce(Vector2.up * hoverForce, ForceMode.Acceleration);
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Pool Cue")
+        {
+            //rb.AddForce(transform.forward * thrust);
+            other.rigidbody.AddForce(Vector2.up * hoverForce, ForceMode.Acceleration);
+
+        }
+    }
+}
+
+
