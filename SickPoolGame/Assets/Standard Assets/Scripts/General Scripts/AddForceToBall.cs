31073d41a284f5bc2890bf952dacdeb9bf307e05
xXJohnCenaXx <thomas@potatoplanet.org>
2015-12-07 12:45:23 -0800

    menus
    added menu scene, and credits picture

--- SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/AddForceToBall.cs
+++ SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/AddForceToBall.cs
@@ -7,28 +7,32 @@ public class AddForceToBall : MonoBehaviour {
     //public float thrust;
     public Rigidbody rb;
     public Vector2 speed;
+    public bool simulated = true;
     void Start () {
         rb = GetComponent<Rigidbody>();
     }
 	
 	void Update () {
+
+
+
         if (Input.GetKeyDown("p"))
         {
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1000f);
         }
 
-        speed = new Vector2(rb.velocity.x, rb.velocity.y);
+        //speed = new Vector2(rb.velocity.x, rb.velocity.y);
     }
 
    
     void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.name == "White Ball" || col.gameObject.name == "Pool Cue")
-        {
-            //rb.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1000f);
+    
+            rb.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1000f);
             speed = new Vector2(rb.velocity.x, rb.velocity.y);
 
-        }
+        
+          speed = new Vector2(rb.velocity.x, rb.velocity.y);
     }
 }
 
