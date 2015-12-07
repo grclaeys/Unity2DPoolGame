b91028642c995d5597a2bf01e475f4e507a32c96
grclaeys <grclaeys@gmail.com>
2015-12-07 12:45:20 -0800

    added title
    AYY LMAO OAML YYAA

--- SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/AddForceToBall.cs
+++ SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/AddForceToBall.cs
@@ -7,28 +7,38 @@ public class AddForceToBall : MonoBehaviour {
     //public float thrust;
     public Rigidbody rb;
     public Vector2 speed;
+
+
     void Start () {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
+        GameObject myGameObject = new GameObject("whiteBall");
+        Rigidbody gameObjectsRigidBody = myGameObject.AddComponent<Rigidbody>();
+        gameObjectsRigidBody.mass = 5;
     }
 	
 	void Update () {
         if (Input.GetKeyDown("p"))
         {
-            GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1000f);
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10000f);
         }
 
         speed = new Vector2(rb.velocity.x, rb.velocity.y);
     }
 
    
-    void OnCollisionEnter(Collision col)
+    void OnTriggerEnter2D(Collision col)
     {
-        if (col.gameObject.name == "White Ball" || col.gameObject.name == "Pool Cue")
-        {
-            //rb.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1000f);
-            speed = new Vector2(rb.velocity.x, rb.velocity.y);
+        GameObject myGameObject = new GameObject("whiteBall");
+        Rigidbody rb = myGameObject.AddComponent<Rigidbody>();
+        rb.mass = 5;
 
-        }
+        //if (col.gameObject.name == "White Ball" || col.gameObject.name == "Pool Cue")
+        //{
+        //rb.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1000f);
+
+        speed = new Vector2(rb.velocity.x, rb.velocity.y);
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10000f);
+        //}
     }
 }
 
