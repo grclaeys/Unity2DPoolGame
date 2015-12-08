f37819889e491b6be69c09e99325afbd5182b252
grclaeys <grclaeys@gmail.com>
2015-12-08 12:40:59 -0800

    buttons?

---
+++ SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/OpenGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Application.LoadLevel("poolTable");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void onGui()
+    {
+        Application.LoadLevel("PoolTable");
+    }
+}
