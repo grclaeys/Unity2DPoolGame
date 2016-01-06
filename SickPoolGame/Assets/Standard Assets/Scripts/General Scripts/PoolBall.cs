63d5ca70688792300300d5a178aa4195bcef449c
michaelPoul <michaelpoulain@msn.com>
2016-01-06 14:46:22 -0800

    Pool Ball base class nearly finished

--- SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/PoolBall.cs
+++ SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/PoolBall.cs
@@ -9,12 +9,22 @@ using System.Collections;
 /// based on the method OnScore
 /// </summary>
 public abstract class PoolBall : MonoBehaviour {
-    public Rigidbody2D ballBody;
+    const string TABLE_HOLE_TAG = "Table Hole";
+    const string POOL_BALL_TAG = "asdf";
 
     void Start()
     {
-        ballBody = GetComponent<Rigidbody2D>();
+        
     }
+
+    public void Update()
+    {
+        if (Input.GetKey("q"))
+        {
+            Debug.Log("Pool Ball Update");
+        }
+    }
+
 
     /// <summary>
     /// Handles pool ball collisions
@@ -41,15 +51,27 @@ public abstract class PoolBall : MonoBehaviour {
         averagePoint.Scale(new Vector2(1.0f / (collision.contacts.Length), 
                            1.0f / (collision.contacts.Length)));
 
-        // gets the line along which the collision occurs
-        // the angle of incidence = the angle between this line and the bodies velocity
-        //Vector2 collisionLine = GetComponent<Rigidbody2D>().position - averagePoint;
-        //Vector2 collisionNormal = new Vector2();
+        // gets the line normal to the collision
         Vector3 collisionNormalTemp = Vector3.Cross(averagePoint - self.position, new Vector3(0, 0, 1));
-        
+        Debug.Log(string.Format("Original Velocity: {0}\tFinal: {1}", 
+                                self.velocity.ToString(), Vector2.Reflect(self.velocity, collisionNormalTemp).ToString()));
+
         // reflects the velocity of this rigid body about the collision line
         self.velocity = Vector2.Reflect(self.velocity, collisionNormalTemp);
-        
+
+        // If the collision was with one of the tables holes, call on score
+        if (collision.transform.tag.CompareTo(TABLE_HOLE_TAG) == 0)
+        {
+            OnScore();
+        }
+
+        // May not be needed as each ball will update its own velocity
+        // Otherwise, if the collision was with another ball, update the other balls velocity
+        //else if (collision.collider.tag.CompareTo(POOL_BALL_TAG) == 0)
+        //{
+
+        //}
+        // Otherwise, the collision was with the table, so no extra updates need to be done
     }
 
     /// <summary>
--- SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/WhiteBall.cs
+++ SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/WhiteBall.cs
@@ -2,19 +2,40 @@
 using System.Collections;
 
 public class WhiteBall : PoolBall {
-	// Update is called once per frame
-	void Update () {
-        //Debug.Log(ballBody.ToString());
-        if (Input.GetKeyDown("a"))
+    public new void Update()
+    {
+        base.Update();
+
+        Vector2 newVelocity = GetComponent<Rigidbody2D>().velocity;
+
+        if (Input.GetKey("a"))
         {
-            ballBody.velocity.Set(-1, 0);
-            Debug.Log(ballBody.velocity.ToString());
-            //Debug.Log("A is down");
+            newVelocity.x = -1;
+            GetComponent<Rigidbody2D>().velocity = newVelocity;
         }
-	}
+        if (Input.GetKey("d"))
+        {
+            newVelocity.x = 1;
+            GetComponent<Rigidbody2D>().velocity = newVelocity;
+        }
+        if (Input.GetKey("w"))
+        {
+            newVelocity.y = 1;
+            GetComponent<Rigidbody2D>().velocity = newVelocity;
+        }
+        if (Input.GetKey("s"))
+        {
+            newVelocity.y = -1;
+            GetComponent<Rigidbody2D>().velocity = newVelocity;
+        }
+    }
+
+
 
     protected override void OnScore() 
     {
-
+        // When the white ball falls in a hole, deduct 1 from the total score
+        // and move the whiteball back to the center of the pool table
+        Debug.Log("Score!");
     }
 }
