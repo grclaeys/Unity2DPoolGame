dad02496f9b06bd39962a0ea31ecd12c5987dc5e
grclaeys <grclaeys@gmail.com>
2016-01-07 13:00:21 -0800

    something?

---
+++ SickPoolGame/Assets/Standard Assets (Mobile)/Scripts/printText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class printText : MonoBehaviour {
+    int score = 0;
+
+	// Use this for initialization
+	void Start () {
+        GameObject newGO = new GameObject("myText");
+        newGO.transform.SetParent(this.transform);
+
+        Text myText = newGO.AddComponent<Text>();
+        myText.text = "Score: " + score;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
