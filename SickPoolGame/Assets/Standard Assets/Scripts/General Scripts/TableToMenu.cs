2c591c2e0118dfbaf84a368a53bb583ec9b4965e
michaelPoul <michaelpoulain@msn.com>
2016-01-07 23:18:22 -0800

    Adding back in useful files

---
+++ SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/TableToMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TableToMenu : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.LoadLevel("MainMenu");
+    }
+}
+
---
+++ SickPoolGame/Assets/Standard Assets/Scripts/General Scripts/creditsToMain.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class creditsToMain : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.LoadLevel("mainMenu");
+        }
+    }
+}
