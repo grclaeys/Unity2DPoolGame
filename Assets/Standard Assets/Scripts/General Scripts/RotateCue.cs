09b5d8f5d31514e6c196b2ac894c02e6d45f923e
michaelPoul <michaelpoulain@msn.com>
2016-01-12 23:12:30 -0800

    Moved around pool cue scripts, working on pool cue ball launcher

--- Assets/Standard Assets/Scripts/General Scripts/RotateCue.cs
+++ Assets/Standard Assets/Scripts/General Scripts/RotateCue.cs
@@ -10,7 +10,7 @@ public class RotateCue : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
-	
+	    
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Standard Assets/Scripts/General Scripts/WhiteBallShooter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+/// <summary>
+/// When attached to an object, the object can be rotated by scrolling the mouse
+/// wheel and holding left click will shoot out temporary white balls
+/// </summary>
+public class WhiteBallShooter : MonoBehaviour {
+    const float SCROLL_SCALE = 3.0f;
+
+    private IList<int> shotBalls;
+
+	void Start ()
+    {
+        shotBalls = new ArrayList();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float scroll = 0.0f;
+        if (Input.GetButton("Fire1"))
+        {
+            Debug.Log("left");
+            scroll += 1.0f;
+        }
+        if (Input.GetButton("Fire2"))
+        {
+            scroll -= 1.0f;
+        }
+        GetComponent<Transform>().Rotate(Vector3.forward, scroll * SCROLL_SCALE);
+
+        if (Input.GetButton("Fire3"))
+        {
+            Debug.Log("Shooting balls");
+
+        }
+	}
+}
